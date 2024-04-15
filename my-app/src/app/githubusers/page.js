3599c9e8bf9d74d8025efa845adce422c0bdf500
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-04-15 16:51:51 +0700

    Dynamic Routes and Param Props

--- my-app/src/app/githubusers/page.js
+++ my-app/src/app/githubusers/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const fetchGitHubUsers = async () => {
   const res = await fetch("https://api.github.com/search/users?q=greg");
   const json = await res.json();
@@ -9,8 +11,47 @@ const GitHubUsersPage = async () => {
   console.log(users);
 
   return (
-    <div>
-      <h1>GitHub Users Page</h1>
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>URL</th>
+            <th>Repos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length ? (
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={user.avatar_url} />
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">{user.login}</div>
+                    <div className="text-sm opacity-50">{user.id}</div>
+                  </div>
+                </td>
+                <td>
+                  <Link href={user.html_url} className="btn btn-link">
+                    View on GitHub
+                  </Link>
+                </td>
+                <th>Go to Repos</th>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>Not Found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -18,15 +59,5 @@ const GitHubUsersPage = async () => {
 export default GitHubUsersPage;
 
 /* 
-การดึงข้อมูล (Data Fetching)
 
-มาพูดถึงการดึงข้อมูลใน React server component กันสักหน่อย ปกติแล้ว ใน React client component เราจะใช้ useEffect สำหรับการดึงข้อมูล อย่างไรก็ตาม ในกรณีนี้ เราต้องการดึงข้อมูลผู้ใช้ GitHub ใน server component เป้าหมายของเราคือการดึงรายชื่อผู้ใช้ GitHub เมื่อคลิกลิงก์ "GitHub Users"
-
-ในการทำเช่นนี้ เราจะต้องสร้าง route สำหรับ "GitHub Users" ภายใต้ 'app' เราจะสร้างโฟลเดอร์ใหม่ชื่อ 'githubusers' และภายในนั้น สร้างไฟล์ชื่อ 'page.jsx' จากนั้นเราจะสร้าง component 'GitHubUsersPage' เมื่อบันทึกแล้ว คุณควรจะถูกนำไปยังหน้าที่เหมาะสมเมื่อคลิกที่ลิงก์ "GitHub Users"
-
-ขั้นตอนต่อไปคือการดึงข้อมูลผู้ใช้ GitHub ซึ่งสามารถทำได้โดยการเข้าถึง GitHub API ผ่าน 'api.github.com/search/users' และเพิ่ม username ต่อท้าย URL เช่น http://api.github.com/search/users?q=greg API จะส่งคืนผลลัพธ์รายการ ซึ่งรวมถึง array 'items' ที่เก็บ array ของผู้ใช้ GitHub
-
-ในการดึงข้อมูลนี้ เราจะสร้างฟังก์ชันชื่อ 'fetchGitHubUsers'
-
-สิ่งสำคัญที่ต้องทราบคือ เนื่องจากนี่เป็น server component จะไม่มีอะไรแสดงใน developer tools ของเบราว์เซอร์ เพราะการดำเนินการทั้งหมดเกิดขึ้นบนฝั่ง server ดังนั้น เราจึงต้องตรวจสอบ server log ในเทอร์มินัลแทน อย่างไรก็ตาม หากมีการระบุ 'useClient' ที่ด้านบนของ component console.log จะปรากฏในเบราว์เซอร์
 */
