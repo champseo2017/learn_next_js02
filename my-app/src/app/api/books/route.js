0ca9d7453108e97b4416af97fe15adcbb0563e1c
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-04-27 23:44:31 +0700

    add getBooks

--- my-app/src/app/api/books/route.js
+++ my-app/src/app/api/books/route.js
@@ -4,11 +4,3 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   return NextResponse.json(books);
 }
-
-/* 
-- เรานำเข้าข้อมูลหนังสือจากไฟล์ `data.json` และนำเข้า `NextResponse` จาก `next/server`
-- เรากำหนด async function ชื่อ `GET` เพื่อจัดการ GET request
-- ภายใน function เราส่งคืน `NextResponse.json(books)` ซึ่ง `books` คือข้อมูลจากไฟล์ JSON ของเรา
-
-เมื่อมีการส่ง GET request ไปที่ `/api/books` Next.js จะเรียก function `GET` ในไฟล์ `route.js` และส่งคืนข้อมูลหนังสือในรูปแบบ JSON กลับไปยัง client
-*/
